a846bf8c3cdea963b5b65323a431c11e99a4b816
FedericoDeniard <fededeniard@gmail.com>
2024-08-31 15:06:02 -0300

    Validate fix

--- src/controllers/functions.js
+++ src/controllers/functions.js
@@ -1,10 +1,20 @@
-const { check } = require("express-validator");
+const { v4: uuidv4, validate: validateUUID } = require("uuid");
+
 const validateForm = (form) => {
-  let checkId = form.id.isUUID();
-  let checkUsername = form.username.isLength({ min: 4, max: 10 });
-  let checkTime = form.time.isNumeric() && form.time > 10;
-  let checkDate =
-    form.date.isNumeric() && form.date >= Date.now() - 5 * 60 * 1000;
+  const isUUID = (id) => validateUUID(id);
+  const isLength = (username) => username.length >= 4 && username.length <= 10;
+  const isNumeric = (value) => !isNaN(value);
+  const isGreaterThan = (value, min) => value > min;
+  const isRecentDate = (date, minutes) =>
+    date >= Date.now() - minutes * 60 * 1000;
+
+  const checkId = isUUID(form.id);
+  const checkUsername = isLength(form.username);
+  const checkTime =
+    isNumeric(form.time) && isGreaterThan(parseFloat(form.time), 10);
+  const checkDate =
+    isNumeric(form.date) && isRecentDate(parseFloat(form.date), 5);
+
   return checkId && checkUsername && checkTime && checkDate;
 };
 
