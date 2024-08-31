933fc61c98ef82543cafc27c1fbc2de9585cc6b4
FedericoDeniard <fededeniard@gmail.com>
2024-08-31 14:53:32 -0300

    port

--- src/app.js
+++ src/app.js
@@ -70,5 +70,5 @@ app.get("/leaderboard", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log("Server running on port " + PORT));
 module.exports = app;
