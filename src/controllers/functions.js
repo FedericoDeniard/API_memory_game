0a23f4140cad8e3fc49f1d1e2693437f3b4ad689
FedericoDeniard <fededeniard@gmail.com>
2024-08-31 15:27:06 -0300

    Time

--- src/controllers/functions.js
+++ src/controllers/functions.js
@@ -5,13 +5,15 @@ const validateForm = (form) => {
   const isLength = (username) => username.length >= 4 && username.length <= 10;
   const isNumeric = (value) => !isNaN(value);
   const isGreaterThan = (value, min) => value > min;
-  const isRecentDate = (date, minutes) =>
-    date >= Math.floor(Date.now() / 1000) - minutes * 60;
+  const isRecentDate = (date, minutes) => {
+    const now = Math.floor(Date.now() / 1000);
+    return date >= now - minutes * 60;
+  };
 
   const checkId = isUUID(form.id);
   const checkUsername = isLength(form.username);
   const checkTime =
-    isNumeric(form.time) && isGreaterThan(parseFloat(form.time), 10);
+    isNumeric(form.time) && isGreaterThan(parseFloat(form.time), 10000);
   const checkDate =
     isNumeric(form.date) && isRecentDate(parseFloat(form.date), 5);
   let error;
