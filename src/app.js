346e628f693bf68d2011e86bb9610e722779c0c3
FedericoDeniard <fededeniard@gmail.com>
2024-09-04 20:35:07 -0300

    config

--- src/app.js
+++ src/app.js
@@ -1,104 +1,108 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-const { User } = require("./models/schemas");
+import { User } from './models/schemas.js'
+import { validateForm } from './controllers/functions.js'
 
-mongoose.connect(process.env.MONGO_URI);
+dotenv.config()
 
-const { validateForm } = require("./controllers/functions");
+mongoose.connect(process.env.MONGO_URI)
 
-const app = express();
+const app = express()
 const corsOptions = {
-  origin: "https://federicodeniard.github.io",
-  methods: "GET,POST",
-  allowedHeaders: "Content-Type",
-};
-app.use(cors(corsOptions));
-app.use(express.json());
+  origin: 'https://federicodeniard.github.io',
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type'
+}
+app.use(cors(corsOptions))
+app.use(express.json())
 
-app.post("/leaderboard/new_record", async (req, res) => {
+app.post('/leaderboard/new_record', async (req, res) => {
   try {
-    const { id, username, time, date } = req.body;
-    let form = { id, username, time, date };
+    const { id, username, time, date } = req.body
+    const form = { id, username, time, date }
     if (!validateForm(form)) {
-      return res.status(400).send({ message: "Invalid form" });
+      return res.status(400).send({ message: 'Invalid form' })
     }
 
-    let user = await User.findOne({ id });
+    const user = await User.findOne({ id })
 
     if (!user) {
-      const user = new User({
+      const newUser = new User({
         id,
         username,
         time,
-        date,
-      });
-      const data = await user.save();
-      res.status(201).send(data);
+        date
+      })
+      const data = await newUser.save()
+      res.status(201).send(data)
     } else {
       await User.findOneAndUpdate(
         { id },
         {
           $set: {
             time,
-            date,
-          },
+            date
+          }
         },
         { new: true }
-      );
+      )
 
-      res.status(200).send({ message: "Record updated" });
+      res.status(200).send({ message: 'Record updated' })
     }
   } catch (error) {
-    console.error("Error saving record:", error);
-    res.status(500).send(error);
+    console.error('Error saving record:', error)
+    res.status(500).send(error)
   }
-});
+})
 
-app.get("/top-leaderboard", async (req, res) => {
+app.get('/top-leaderboard', async (req, res) => {
   try {
     const data = await User.find()
       .sort({ time: 1 })
       .limit(10)
-      .select("id username time date");
+      .select('id username time date')
     if (data.length === 0) {
-      res.status(404).send({ message: "No records found" });
+      res.status(404).send({ message: 'No records found' })
     }
     const processedData = data.map((user) => ({
       id: user.id.slice(-4),
       username: user.username,
       time: user.time,
-      date: user.date,
-    }));
-    res.status(200).send(processedData);
+      date: user.date
+    }))
+    res.status(200).send(processedData)
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-});
+})
 
-app.get("/last-leaderboard", async (req, res) => {
+app.get('/last-leaderboard', async (req, res) => {
   try {
     const data = await User.find()
       .sort({ date: -1 })
       .limit(10)
-      .select("id username time date");
+      .select('id username time date')
     if (data.length === 0) {
-      res.status(404).send({ message: "No records found" });
+      res.status(404).send({ message: 'No records found' })
     }
     const processedData = data.map((user) => ({
       id: user.id.slice(-4),
       username: user.username,
       time: user.time,
-      date: user.date,
-    }));
-    res.status(200).send(processedData);
+      date: user.date
+    }))
+    res.status(200).send(processedData)
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-});
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
-module.exports = app;
+// Login
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log('Server running on port ' + PORT))
+
+export default app
--- src/models/schemas.js
+++ src/models/schemas.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose'
 
 const UserRecord = new mongoose.Schema({
   id: String,
   username: String,
   time: Number,
-  date: Number,
-});
+  date: Number
+})
 
-const User = mongoose.model("User", UserRecord);
-module.exports = { User };
+const User = mongoose.model('User', UserRecord)
+
+export { User }
